892d1ae2bf7bd162d5a938e9cc70de2a77f617bd
Erfan Abdi <erfangplus@gmail.com>
2022-07-16 17:24:20 +0430

    yggdrasilx: Add and setup safetye logo

--- yggdrasilx/firmware.mk
+++ yggdrasilx/firmware.mk
@@ -2,8 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),yggdrasilx)
 
-RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
+RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*.img)
 $(foreach f, $(notdir $(RADIO_FILES)), \
     $(call add-radio-file,radio/$(f)))
+
+ifeq ($(strip $(VOLLA_BUILD_FLAVOR)),)
+    $(call add-radio-file,radio/logo.bin)
+else
+    $(call add-radio-file,$(VOLLA_BUILD_FLAVOR)/logo.bin)
+endif
 
 endif
